2ef26a5c27011a1c0b88eae30623cd9096c00862
bethurban <beth.urban@gmail.com>
2019-07-29 14:04:22 -0400

    Create Weather container, return in App

---
+++ src/containers/Weather.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Weather extends Component {
+  render() {
+    return (
+      <div>
+        Weather container
+      </div>
+    )
+  }
+}
+
+export default Weather;
