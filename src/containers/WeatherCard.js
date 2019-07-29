7f19f44f7f7124836a98018a8a7590bfb4496a9f
bethurban <beth.urban@gmail.com>
2019-07-29 17:47:49 -0400

    Parse this.props.day to string - 'Tue Jul 30 2019', for example.

--- src/containers/WeatherCard.js
+++ src/containers/WeatherCard.js
@@ -9,26 +9,30 @@ class WeatherCard extends Component {
 
   render() {
 
-      var image
+    var image
 
-      if (this.props.weather.id === 800) {
-        image = <img src={Clear} alt="Prince" height="200" width="200"></img>
-      } else if (this.props.weather.id >= 801 && this.props.weather.id <= 804) {
-        image = <img src={Clouds} alt="Prince" height="200" width="200"></img>
-      } else if (this.props.weather.id >= 600 && this.props.weather.id <= 622) {
-        image = <img src={Snow} alt="Prince" height="200" width="200"></img>
-      } else if (this.props.weather.id >= 300 && this.props.weather.id <= 321) {
-        image = <img src={Rain} alt="Prince" height="200" width="200"></img>
-      } else if (this.props.weather.id >= 500 && this.props.weather.id <= 531) {
-        image = <img src={Rain} alt="Prince" height="200" width="200"></img>
-      } else if (this.props.weather.id >= 200 && this.props.weather.id <= 232) {
-        image = <img src={Storm} alt="Prince" height="200" width="200"></img>
-      }
+    const date = () => {
+      var day = new Date(this.props.day).toString().split(" ")
+      return day.slice(0,4).join(" ")
+    }
 
+    if (this.props.weather.id === 800) {
+      image = <img src={Clear} alt="Prince" height="200" width="200"></img>
+    } else if (this.props.weather.id >= 801 && this.props.weather.id <= 804) {
+      image = <img src={Clouds} alt="Prince" height="200" width="200"></img>
+    } else if (this.props.weather.id >= 600 && this.props.weather.id <= 622) {
+      image = <img src={Snow} alt="Prince" height="200" width="200"></img>
+    } else if (this.props.weather.id >= 300 && this.props.weather.id <= 321) {
+      image = <img src={Rain} alt="Prince" height="200" width="200"></img>
+    } else if (this.props.weather.id >= 500 && this.props.weather.id <= 531) {
+      image = <img src={Rain} alt="Prince" height="200" width="200"></img>
+    } else if (this.props.weather.id >= 200 && this.props.weather.id <= 232) {
+      image = <img src={Storm} alt="Prince" height="200" width="200"></img>
+    }
 
     return (
       <div>
-        <h2>{this.props.day}</h2>
+        <h2>{date()}</h2>
         {image}
         <h3>{this.props.weather.main}</h3>
         <p>{this.props.weather.description}</p>
