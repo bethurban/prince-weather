4bbbf4529dae68ed0b6bf5257391084bbff9243c
bethurban <beth.urban@gmail.com>
2019-07-30 11:05:06 -0400

    Link to font stylesheet. Add webfont files. Format font in App CSS.

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Prince Predicts the Weather</h1>
         <Weather weather={this.state.weather} />
       </div>
     );
